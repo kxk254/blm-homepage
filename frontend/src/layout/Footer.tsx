f559e7c41ae51f09991ac76c3cd754eb704f245c
konno <konno.kenji@gmail.com>
2026-02-18 06:54:47 +0000

    2

--- frontend/src/layout/Footer.tsx
+++ frontend/src/layout/Footer.tsx
@@ -1,3 +1,48 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./Footer.module.css";
+
 export default function Footer() {
-  return <div>FOOOOOTER</div>;
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <Link
+          href="https://www.instagram.com/bluemillefeuille2001"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/asset/instagram.png"
+            width={24}
+            height={24}
+            alt="Instagram"
+          />
+        </Link>
+        <Link
+          href="https://www.facebook.com/profile.php?id=100070949671198"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/asset/facebook.png"
+            width={24}
+            height={24}
+            alt="Facebook"
+          />
+        </Link>
+        <Link
+          href="https://mdfshop.base.shop/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="/asset/base.png" width={24} height={24} alt="Base" />
+        </Link>
+      </div>
+      <div className={styles.footerLinks}>
+        <Link href="/">ABOUT</Link>
+        <Link href="/">CONTACT</Link>
+      </div>
+      <p>&copy; 2026 Blue Mille Feuille All Rights Reserved.</p>
+    </footer>
+  );
 }
