3feff871194fd9bd10fbc4c570d8603de44c62a2
konno <konno.kenji@gmail.com>
2026-02-18 07:30:22 +0000

    2

---
+++ frontend/src/components/ui/ItemCard.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+type CardProps = {
+  id: string;
+  productType: string;
+  productColor: string;
+  productName: string;
+  productDescription: string;
+  productPrice: number;
+  imageSrc: string;
+  link?: string;
+};
+
+export default function ItemCard({
+  id,
+  productType,
+  productColor,
+  productName,
+  productDescription,
+  productPrice,
+  imageSrc,
+  link,
+}: CardProps) {
+  return (
+    <div>
+      <div>
+        <Link href={link}>
+          <Image />
+        </Link>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
