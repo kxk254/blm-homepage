bb44d15b330c4daedf2e69bc0a18f3988f958d13
konno <konno.kenji@gmail.com>
2026-02-23 14:03:02 +0000

    2

--- frontend/app/(home)/contact/page.tsx
+++ frontend/app/(home)/contact/page.tsx
@@ -1,8 +1,22 @@
 import styles from "./page.module.css";
 import ContactCard from "@/src/components/card/ContactCard";
-import { contact } from "@/data/ContactCard";
+{
+  /* import { contact } from "@/data/ContactCard"; */
+}
 
-export default function Contact() {
+async function getContact() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/contact`,
+    {
+      cache: "no-store",
+    },
+  );
+  const contact = await res.json();
+  return contact;
+}
+
+export default async function Contact() {
+  const contact = await getContact();
   return (
     <div className={styles.content}>
       <ContactCard
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "10.66.66.4",
+        port: "8000",
+        pathname: "/media/**",
+      },
+      { protocol: "https", hostname: "blmf.jp" },
+    ],
+  },
 };
 
 export default nextConfig;
