a3b9ad4b17afc886b9fe3970b257519a2ac824a8
konno <konno.kenji@gmail.com>
2026-02-23 06:16:59 +0000

    2

--- backend/blmf/urls.py
+++ backend/blmf/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("contact/", views.ServiceCardViewSet.as_view()),
+    path("contact/", views.ServiceCardViewSet.as_view({"get": "list"}), name="contact"),
 ]
