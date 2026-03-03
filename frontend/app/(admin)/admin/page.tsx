a659a50b0e494bc5bf1c841d8eaad133a9cca0cc
konno <konno.kenji@gmail.com>
2026-03-03 13:46:37 +0000

    2

--- frontend/app/(admin)/admin/page.tsx
+++ frontend/app/(admin)/admin/page.tsx
@@ -1,9 +1,10 @@
 import HomeCard from "@/src/components/forms/admin/homeCard";
+import HomeList from "@/src/components/admin/HomeList";
 
 export default function Admin() {
   return (
     <div>
-      <HomeCard />
+      <HomeList />
     </div>
   );
 }
