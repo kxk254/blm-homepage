ed500ae4c68888f06a249abc916ca8029e11bc76
konno <konno.kenji@gmail.com>
2026-03-07 09:36:56 +0000

    2

--- backend/blmf/urls.py
+++ backend/blmf/urls.py
@@ -1,16 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
-urlpatterns = [
-    path(
-        "contact/", views.ContactPropsViewSet.as_view({"get": "list"}), name="contact"
-    ),
-    path("about/", views.ServiceCardViewSet.as_view({"get": "list"}), name="about"),
-    path("home/", views.CardPropsViewSet.as_view({"get": "list"}), name="home"),
-]
+router = DefaultRouter()
+router.register(r"home", views.CardPropsViewSet, basename="home")
+router.register(r"about", views.ServiceCardViewSet, basename="about")
+router.register(r"contact", views.ContactPropsViewSet, basename="contact")
+urlpatterns = router.urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- frontend/app/(admin)/admin/edit/[id]/page.tsx
+++ frontend/app/(admin)/admin/edit/[id]/page.tsx
@@ -1,4 +1,20 @@
-export default function EditPage({ params }) {
-  const { id } = params;
-  return <div>Edit Product {id}</div>;
+"use client";
+
+import { useState, useEffect, use } from "react";
+import CardProps from "@/data/types";
+
+export default function EditPage({ params }: CardProps) {
+  const { id } = use(params);
+  const [user, setUser] = useState<any>(null);
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/home/${id}`, {})
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+  return (
+    <div>
+      Edit Product {user?.["product_color"]}
+      {id}
+    </div>
+  );
 }
